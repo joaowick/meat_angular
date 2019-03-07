d4bb3509ddea29039b1e12e88b5eeb7f8cf2d63b
joaowick <joaotorrez14@gmail.com>
2019-03-07 20:33:06 -0300

    Segurança Front-End

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -11,7 +11,7 @@ import { LoginService } from "app/security/login/login.service";
 @Injectable()
 export class OrderService {
     
-    constructor (private cartService: ShoppingCartService, private http: HttpClient, private loginService: LoginService){}
+    constructor (private cartService: ShoppingCartService, private http: HttpClient){}
 
     cartItems(): CartItem[]{
         return this.cartService.items
@@ -34,11 +34,7 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        let headers = new HttpHeaders()
-        if(this.loginService.isLoggedIn()){
-            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
-        }
-        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers: headers})
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
             .map(order => order.id)
     }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,8 @@ import { NotificationService } from "./messages/notification.service";
 import { LoginService } from "app/security/login/login.service";
 import { LoggedInGuard } from "app/security/loggedin.guard";
 import { LeaveOrderGuard } from "app/order/leave-order.guard";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "app/security/auth.interceptor";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -32,7 +34,8 @@ export class SharedModule {
                 NotificationService, 
                 LoginService, 
                 LoggedInGuard,
-                LeaveOrderGuard
+                LeaveOrderGuard,
+                {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
             ]
         }
     }
