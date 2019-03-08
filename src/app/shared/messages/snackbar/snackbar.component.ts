09b3b09c311e57171e7ce9af3758214c8b0299ae
joaowick <joaotorrez14@gmail.com>
2019-03-07 21:52:38 -0300

    Aulas Extras: Error Handling

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -36,9 +36,10 @@ export class SnackbarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.notificationService.notifier.do(message=>{
-        this.message = message
-        this.snackVisibility = 'visible'
+    this.notificationService.notifier
+    .do(message=>{
+      this.message = message
+      this.snackVisibility = 'visible'
     }).switchMap(message => Observable.timer(3000))
       .subscribe(timer => this.snackVisibility = 'hidden')
   }
